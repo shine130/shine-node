2dcf5cd81f50d69de4f834b07b50a19884b3ff4f
shine <shine130@vip.qq.com>
2019-04-01 14:38:42 +0800

    完成socket.IO

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
 import path from 'path'
+import socketIO from 'socket.io'
 
 const app = express()
 
@@ -13,6 +14,19 @@ app.get('/',(request,response) => {
     response.render('index')
 })
 
-app.listen(3000,() => {
+let server = app.listen(3000,() => {
     console.log('Listen port:3000')
+})
+
+let io = socketIO(server)
+
+io.on('connection',(socket) => {
+    console.log('user connected')
+    socket.on('disconnect',() => {
+        console.log('user disconnected')
+    })
+    socket.on('message',(message) => {
+        console.log(message)
+        io.emit('message',message)
+    })
 })
