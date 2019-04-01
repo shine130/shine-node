989f2661f7fbcbef6705c2d575370166ee36f1b8
shine <shine130@vip.qq.com>
2019-04-01 17:54:45 +0800

    完成注册用户

---
+++ controllers/UserController.js
@@ -0,0 +1,20 @@
+const User = require('../models/user')
+
+const store = (request,response) => {
+  const userName = request.body.userName
+  const password = request.body.password
+
+  const user = new User({
+    userName,
+    password
+  })
+
+  user.save()
+    .then( () => response.send({ message: '注册成功'}))
+    .catch(error => response.send(error))
+
+}
+
+module.exports = {
+  store
+}
